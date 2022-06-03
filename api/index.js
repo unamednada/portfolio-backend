08ff9d1dcb191d6e4cc92340098e286aa86452e8
unamednada <dias.gustavo.dias@gmail.com>
2022-06-03 19:16:38 -0300

    API get and post routes working ok. query ok. front-end ok

--- api/index.js
+++ api/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 
 const { Project } = require('../models');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
-app.post('/project', async (req, res) => {
-  const project = await Project.create(req.body);
+app
+  .post('/projects', async (req, res) => {
+    const project = await Project.create(req.body);
 
-  return res.status(201).json(project);
-});
+    return res.status(201).json(project);
+  })
+  .get('/projects', async (req, res) => {
+    const projects = await Project.findAll();
+
+    return res.status(200).json(projects);
+  });
 
 app.listen(port, () => { console.log(`Servidor online na porta ${port}`); });
